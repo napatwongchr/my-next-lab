8ebd12054281e1aef8a6d6592234f70dde1c2eaa
Napat <napat.wongchr@gmail.com>
2022-11-30 08:23:20 +0700

    feat: add tailwind lab page

---
+++ pages/tailwind-lab.tsx
@@ -0,0 +1,11 @@
+export default function TailwindLabHomepage() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <h1 className="text-5xl font-bold">
+        Hi! <span className="text-purple-700">Welcome</span>{" "}
+        <span className="text-purple-600">to</span>{" "}
+        <span className="text-purple-400">Tailwind Lab </span>
+      </h1>
+    </div>
+  );
+}
